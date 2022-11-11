16d500f6ad80af6d84316ae2760787fc2473b116
MH <myungh5@MHui-MacBookAir.local>
2022-11-12 07:59:39 +0900

    rebase wip

--- repush.ts
+++ repush.ts
@@ -1,7 +1,3 @@
 const b: string = 'I will repush this file'
-<<<<<<< HEAD
-const c: string = 'I modified this file' 
-=======
 const c: string = 'I modified this file'
 const d: string = 'I fixed it again'
->>>>>>> 2964361 (fix-again)
