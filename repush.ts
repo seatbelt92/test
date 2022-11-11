ac2a81daee20de8343305762bdf37075ec9d885e
MH <myungh5@MHui-MacBookAir.local>
2022-11-12 07:44:54 +0900

    fix-again

--- repush.ts
+++ repush.ts
@@ -1,2 +1,7 @@
 const b: string = 'I will repush this file'
+<<<<<<< HEAD
 const c: string = 'I modified this file' 
+=======
+const c: string = 'I modified this file'
+const d: string = 'I fixed it again'
+>>>>>>> 2964361 (fix-again)
